e95c54b8f11e2ca8ea6b2f560e741b1ec2fe425f
Ivana Huckova <ivana.huckova@gmail.com>
2019-04-04 14:36:50 +0200

    Set up react component and functionality for register

--- client/src/App.js
+++ client/src/App.js
@@ -8,14 +8,18 @@ import Navbar from './Components/Navbar';
 import Register from './Components/Register';
 import UserList from './Components/UserList';
 import DepartmentList from './Components/DepartmentList';
+import Landing from './Components/Landing';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
-        <Route exact path="/register" render={pr => <egister {...pr} />} />
+        <Route exact path="/" render={pr => <Landing {...pr} />} />
         <Route exact path="/login" render={pr => <Login {...pr} />} />
+        <Route exact path="/register" render={pr => <Register {...pr} />} />
+        <Route exact path="/users" render={pr => <UserList {...pr} />} />
+        <Route exact path="/departments" render={pr => <DepartmentList {...pr} />} />
       </div>
     );
   }
--- client/src/Components/Register.js
+++ client/src/Components/Register.js
@@ -1,5 +1,46 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import axios from 'axios';
 
-export default function Register() {
-  return <div />;
+export default function Register(props) {
+  const usernameRef = useRef();
+  const passwordRef = useRef();
+  const departmentRef = useRef();
+  const [message, setMessage] = useState('');
+
+  const onRegister = e => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:4444/api/register', {
+        username: usernameRef.current.value,
+        password: passwordRef.current.value,
+        department: departmentRef.current.value
+      })
+      .then(res => {
+        setMessage(`${res.data.message}`);
+      })
+      .catch(err => {
+        setMessage(`Error: ${err.response.data.message}`);
+      })
+      .finally(() => props.history.push(`/login`));
+  };
+
+  return (
+    <div>
+      <form onSubmit={onRegister}>
+        <div>
+          username: <input required="required" type="text" ref={usernameRef} />
+        </div>
+        <div>
+          password: <input required="required" type="text" ref={passwordRef} />
+        </div>
+        <div>
+          department: <input required="required" type="text" ref={departmentRef} />
+        </div>
+        <div>
+          <input type="submit" value="submit" />
+        </div>
+        <div>{message}</div>
+      </form>
+    </div>
+  );
 }
